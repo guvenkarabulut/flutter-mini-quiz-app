834dd5cf383243bf6e54f8dd00541f0141771879
Güven Karabulut <56564068+guven07@users.noreply.github.com>
2022-12-31 18:47:01 +0300

    Some Corrections.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
 
+import './quiz.dart';
+import './result.dart';
 /* void main() {
   runApp(myApp());
 } */
@@ -15,13 +15,60 @@ class myApp extends StatefulWidget {
   }
 }
 
+final _questions = const [
+  {
+    'questionText': 'What\'s your favorite color?',
+    'answers': [
+      {'text': 'Black', 'score': 10},
+      {'text': 'Red', 'score': 5},
+      {'text': 'Green', 'score': 3},
+      {'text': 'White', 'score': 1}
+    ]
+  },
+  {
+    'questionText': 'What\'s your favorite animal?',
+    'answers': [
+      {'text': 'Rabbit', 'score': 3},
+      {'text': 'Snake', 'score': 11},
+      {'text': 'Elephant', 'score': 5},
+      {'text': 'Lion', 'score': 9}
+    ]
+  },
+  {
+    'questionText': 'What\'s your favorite food',
+    'answers': [
+      {'text': 'Pizza', 'score': 10},
+      {'text': 'Burger', 'score': 4},
+      {'text': 'Pasta', 'score': 9},
+      {'text': 'Noodles', 'score': 1}
+    ]
+  },
+  {
+    'questionText': 'What\'s your favorite drink',
+    'answers': [
+      {'text': 'Coke', 'score': 10},
+      {'text': 'Pepsi', 'score': 4},
+      {'text': 'Fanta', 'score': 9},
+      {'text': 'Sprite', 'score': 1}
+    ]
+  },
+];
+
 class _myAppState extends State<myApp> {
   var _questionIndex = 0;
-  void _answerQuestion() {
+  var _totalScore = 0;
+
+  void _resetQuiz() {
     setState(() {
-      if (_questionIndex == 3) {
-        _questionIndex = 0;
-      } else {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      if (_questionIndex != _questions.length) {
         _questionIndex = _questionIndex + 1;
       }
     });
@@ -29,24 +76,6 @@ class _myAppState extends State<myApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White']
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'What\'s your favorite food?',
-        'answers': ['Pizza', 'Burger', 'Pasta', 'Salad'],
-      },
-      {
-        'questionText': 'What\'s your favorite drink?',
-        'answers': ['Coke', 'Pepsi', 'Fanta', 'Sprite'],
-      }
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -54,14 +83,13 @@ class _myAppState extends State<myApp> {
           centerTitle: true,
           backgroundColor: Colors.red,
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['questionText']),
-            ...(questions[_questionIndex]['answers'] as List<String>).map((e) {
-              return Answer(_answerQuestion, e);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
